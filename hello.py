5fa00f007c8b18f583d2dfae7d2c34844d31a2bf
Disala Sennath <sennathdesilva@gmail.com>
2026-02-24 22:49:06 +0530

    next commit

--- hello.py
+++ hello.py
@@ -1,4 +1,43 @@
-import requests
+# # my_list = ["Alice", 23, True, "Hi"]
+# # sec = my_list[0:2]
+# # print(sec)
 
-response = requests.get("https://github.com/7amyxl")
-print(response.status_code)
+# person = {
+#     "name": "Disala",
+#     "age": 18,
+#     "status": False
+# }
+# print(person["name"])
+# hello = person["hello"] = "Just get out"
+# print(person)
+
+# my_score = dict(math=40, science=50, english=60)
+# print(my_score)
+
+# my_score.clear()
+# print(my_score)
+
+# my_tuple = ("Hello","World","Bro")
+# print(my_tuple)
+# my_tuple[0] = "Hello2"
+# print(my_tuple[0])
+
+# Empty set (careful!)
+# empty_set = set()  # NOT {} - that's a dict!
+
+# # Set with values - both ways work
+# numbers = {1, 2, 3, 4, 5}
+# my_numbers = {2,3,4,5,6,7}
+# print(my_numbers)
+# fruits = set(["apple", "banana", "orange"])
+
+# # From a list (removes duplicates)
+# scores = [85, 90, 85, 92, 90]
+# unique = set(scores)  # {85, 90, 92}
+# print(unique)
+
+# color = {"Disala","Dave","Michel"}
+# color.add("Green Apple")
+# print(color)
+
+
